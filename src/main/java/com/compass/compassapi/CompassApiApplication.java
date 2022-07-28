c104d5f13f8abe4d3d2cc6365fb2682eef363af7
liushuozhen <18856655661@163.com>
2022-07-28 16:56:00 +0800

    🎉 司南后台管理系统后台初始化

---
+++ src/main/java/com/compass/compassapi/CompassApiApplication.java
@@ -0,0 +1,13 @@
+package com.compass.compassapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompassApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/compass/compassapi/CompassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.compassapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompassApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
