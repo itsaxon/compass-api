f6a67f75f130496666e90b8d49bba2df8bca2f28
liushuozhen <18856655661@163.com>
2022-08-01 14:09:30 +0800

    🐛 解决启动失败的一系列问题

--- web/src/main/java/com/compass/web/App.java
+++ web/src/main/java/com/compass/web/App.java
@@ -1,8 +1,11 @@
 package com.compass.web;
 
+import cn.hutool.core.collection.CollUtil;
+import org.apache.commons.compress.utils.Lists;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import java.util.ArrayList;
 
 /**
  * 应用程序
@@ -14,9 +17,18 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class App {
 
     public static void main(String[] args) {
+//        System.out.println("App starting -------------------------");
+//        SpringApplication.run(App.class, args);
+//        System.out.println("App started -------------------------");
+
         System.out.println("App starting -------------------------");
-        SpringApplication.run(App.class, args);
+        SpringApplication springApplication = new SpringApplication();
+        ArrayList<Class<?>> sources = CollUtil.newArrayList(App.class);
+        springApplication.addPrimarySources(sources);
+
+        springApplication.run(args);
         System.out.println("App started -------------------------");
+
     }
 
 }
